48a6eab68ea3c846f5c086e9c14c90fa333e204a
agsry <agungkdp208@gmail.com>
2023-11-10 16:02:47 +0700

    scsac

---
+++ mahasiswa/irsMahasiswa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IRS Mahasiswa</title>
+</head>
+
+<body>
+    Ini IRS Mahasiswa
+</body>
+
+</html>
