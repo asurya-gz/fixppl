e2c65d3f908b2fdd4e3a6ca3b17dca956dc8d9c4
agsry <agungkdp208@gmail.com>
2023-11-11 18:52:01 +0700

    irs khs

--- mahasiswa/irsMahasiswa.php
+++ mahasiswa/irsMahasiswa.php
@@ -21,7 +21,7 @@
                 <div class="sem1">
                     <label for="semester1">Semester 1</label>
                     <select id="semester1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                        <option value="" disabled selected>Pilih Jumlah SKS</option>
+                        <option value="" disabled selected>Jumlah SKS</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
---
+++ mahasiswa/khsMahasiswa.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman IRS</title>
+    <link rel="stylesheet" href="css/khsMahasiswa.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../dashboardMahasiswa.php">Kembali</a>
+        </li>
+    </ul>
+
+    <div class="container">
+        <div class="kiri">
+            <div class="sem123">
+                <div class="sem1">
+                    <label for="semester1">Semester 1</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester2">Semester 2</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester3">Semester 3</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+            </div>
+            <div class="sem456">
+                <div class="sem1">
+                    <label for="semester4">Semester 4</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester5">Semester 5</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester6">Semester 6</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+            </div>
+            <div class="sem789">
+                <div class="sem1">
+                    <label for="semester7">Semester 7</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester8">Semester 8</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester9">Semester 9</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+        <div class="kanan">
+            <div class="sem123">
+                <div class="sem1">
+                    <label for="semester10">Semester 10</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester11">Semester 11</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester12">Semester 12</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+            </div>
+            <div class="sem456">
+                <div class="sem1">
+                    <label for="semester13">Semester 13</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+                <div class="sem1">
+                    <label for="semester14">Semester 14</label>
+                    <p>Jumlah SKS :</p>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
