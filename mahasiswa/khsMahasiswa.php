c6fdfe988dd35a1446683cc300db7cbb6269c6c3
MhamanK <100416105+MhamanK@users.noreply.github.com>
2023-11-11 21:06:19 +0700

    Update khsMahasiswa.php

--- mahasiswa/khsMahasiswa.php
+++ mahasiswa/khsMahasiswa.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman IRS</title>
-    <link rel="stylesheet" href="css/khsMahasiswa.css">
+    <title>KHS Mahasiswa</title>
+    <link rel="stylesheet" href="css/khsmahasiswa.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
@@ -15,91 +15,26 @@
             <a class="nav-link active" aria-current="page" href="../dashboardMahasiswa.php">Kembali</a>
         </li>
     </ul>
-
     <div class="container">
         <div class="kiri">
-            <div class="sem123">
-                <div class="sem1">
-                    <label for="semester1">Semester 1</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester2">Semester 2</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester3">Semester 3</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-            </div>
-            <div class="sem456">
-                <div class="sem1">
-                    <label for="semester4">Semester 4</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester5">Semester 5</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester6">Semester 6</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-            </div>
-            <div class="sem789">
-                <div class="sem1">
-                    <label for="semester7">Semester 7</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester8">Semester 8</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester9">Semester 9</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-            </div>
-        </div>
-        <div class="kanan">
-            <div class="sem123">
-                <div class="sem1">
-                    <label for="semester10">Semester 10</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester11">Semester 11</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester12">Semester 12</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-            </div>
-            <div class="sem456">
-                <div class="sem1">
-                    <label for="semester13">Semester 13</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-                <div class="sem1">
-                    <label for="semester14">Semester 14</label>
-                    <p>Jumlah SKS :</p>
-                    <p></p>
-                </div>
-            </div>
+            <h2>Kartu Hasil Studi</h2>
+            <form action="#" method="post">
+                <label for="semester">Pilih Semester</label>
+                <select id="semester" name="semester" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    <option value="" disabled selected>Pilih Semester</option>
+                    <?php
+                    for ($i = 1; $i <= 14; $i++) {
+                        echo '<option value="' . $i . '">' . $i . '</option>';
+                    }
+                    ?>
+                </select>
+                <label for="sks">Jumlah SKS</label>
+                <input type="text" id="sks" name="sks" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" readonly>
+                <label for="ip">Nilai IP</label>
+                <input type="text" id="ip" name="ip" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" readonly>
+                <label for="ipk">IPK</label>
+                <input type="text" id="ipk" name="ipk" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" readonly>
+            </form>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
